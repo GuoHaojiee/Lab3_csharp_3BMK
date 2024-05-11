e8bd99e7f4ba6cd623febedb74e71ae793a9323d
Го Хаоцзе <103988645+GuoHaojiee@users.noreply.github.com>
2024-05-12 00:48:54 +0300

    Add files via upload

---
+++ ConsoleApp4/Data/FValues.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp4.Data;
+
+namespace ConsoleApp4.Data
+{
+    public delegate void FValues(double x, ref double y1, ref double y2);
+}
---
+++ ConsoleApp4/main.cs
@@ -0,0 +1,22 @@
+﻿using ConsoleApp4.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    public class main
+    {
+        static void Main() {
+            V1DataArray V1DataArray = new V1DataArray("DataArray", DateTime.Now, 5, 1, 5, true, MyFunctions.MyFunction1);
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(V1DataArray.xArray[i]);
+                Console.WriteLine(1 + (6 - 1) * i / (5 - 1));
+            }
+            Console.ReadLine();
+        }
+    }
+}
