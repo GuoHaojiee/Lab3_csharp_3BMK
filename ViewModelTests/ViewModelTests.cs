99bf080e57d841e1f086f4d89b69d379a17485fc
GuoHaojiee <ghaojie@yandex.ru>
2024-05-14 14:02:10 +0300

    initial commit

--- ViewModelTests/ViewModelTests.cs
+++ ViewModelTests/ViewModelTests.cs
@@ -27,7 +27,7 @@ namespace ViewModelTests
             var fileDialogMock = new Mock<IFileDialog>();
             string expectedFilename = "testData.v1d";
             fileDialogMock.Setup(m => m.OpenFileDialog()).Returns(expectedFilename);
-
+            // Настройка мока, чтобы симулировать выбор пользователя открывать несуществующий файл
             var mockData = new V1DataArray("mockData", DateTime.Now);
 
 
@@ -37,6 +37,25 @@ namespace ViewModelTests
 
             fileDialogMock.Verify(m => m.OpenFileDialog(), Times.Once);
 
+        }
+
+        //Доп. задания  Проверка когда файл не существует
+        [Fact]
+        public void LoadFromFileCommandHandler_FileNotExist_ShouldSendError()
+        {
+            var errorSenderMock = new Mock<IErrorSender>();
+            var fileDialogMock = new Mock<IFileDialog>();
+            string nonexistentFilename = "nonexistent.v1d";// Имитация выбора несуществующего файла
+
+            fileDialogMock.Setup(m => m.OpenFileDialog()).Returns(nonexistentFilename);
+            // то что вы мне спрашивали:
+            // Это настройка мока, чтобы симулировать выбор пользователя открывать несуществующий файл
+            var viewData = new ViewData(errorSenderMock.Object, fileDialogMock.Object);
+
+            viewData.LoadFromFileCommand.Execute(null);
+
+            errorSenderMock.Verify(m => m.SendError(It.IsAny<string>()), Times.AtLeastOnce);
+            // Проверка, что метод отправки ошибки был вызван хотя бы один раз
         }
 
     }
